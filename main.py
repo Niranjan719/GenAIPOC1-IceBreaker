40398ada35a577a7e95b266f94451bb5f600f717
Niranjan <niranjansaikoppisetti@gmail.com>
2025-05-26 22:02:19 +0530

    edited main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Chariii')
+    print_hi('Chariii is sorry')
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
